ceda52e597eb247c079d9ec4649a338526ab39b8
Luan Reis <luanreis2723@gmail.com>
2025-04-09 20:43:45 -0300

    refact(api): refatoração para nova biblioteca para acessar banco de dados, o SQLAlchemy + verificação na adição de produto já existente

--- app.py
+++ app.py
@@ -1,33 +1,55 @@
 import os
-from dotenv import load_dotenv, dotenv_values
+import datetime
+from dotenv import load_dotenv
 from flask import Flask, flash, render_template, redirect, request
-import mysql.connector
+from sqlalchemy import MetaData, create_engine, Table, Column, Integer, String, Float, Enum, DateTime, insert, select, delete, update, desc, or_
+from sqlalchemy.exc import SQLAlchemyError
 from helpers import validateInt, validateFloat, error
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("APP_SECRET_KEY")
-db = mysql.connector.connect(user= os.getenv("DB_USER"), password= os.getenv("DB_PASSWORD"),
-                            host=os.getenv("DB_HOST"), database=os.getenv("DB_SCHEMA"))
-cursor = db.cursor()
+
+CATEGORIAS = [
+    "Cama",
+    "Mesa",
+    "Banho",
+    "Plástico",
+    "Cozinha"
+]
+
+# conectando com banco de dados
+engine = create_engine("sqlite:///estoque.db", echo=True)
+db = MetaData()
+produtosDb = Table("produtos", db,
+    Column("id", Integer, primary_key=True),
+    Column("nome", String(35), nullable=False),
+    Column("quantidade", Integer, nullable=False),
+    Column("valor", Float, nullable=False),
+    Column("categoria", Enum(*CATEGORIAS), nullable=False),
+    Column("last_update", DateTime, onupdate=datetime.datetime.now),
+)
 
 
 @app.route("/")
 def home():
-    query = "SELECT * FROM produtos "
-
     order = request.args.get("order") or "recente"
-    ordering = {"recente": " ",
-                "asc": "ORDER BY valor ASC", 
-                "desc": "ORDER BY valor DESC",
-                "a-z": "ORDER BY nome ASC",
-                "z-a": "ORDER BY nome DESC",
-                "more-quant": "ORDER BY quantidade DESC",
-                "less-quant": "ORDER BY quantidade ASC",}
+    ordering = {"recente": produtosDb.c.last_update,
+                "asc": produtosDb.c.valor, 
+                "desc": desc(produtosDb.c.valor),
+                "a-z": produtosDb.c.nome,
+                "z-a": desc(produtosDb.c.nome),
+                "more-quant": produtosDb.c.quantidade,
+                "less-quant": desc(produtosDb.c.quantidade)}
     
-    query += ordering[order]
-    cursor.execute(query)
-    produtos = cursor.fetchall()
+    stmt = select(produtosDb).order_by(ordering[order])
+    with engine.connect() as conn:
+        try:
+            produtos = conn.execute(stmt).fetchall()
+        except SQLAlchemyError as e:
+            flash(f"Ocorreu um erro ao buscar produtos: {e}")
+            produtos = {}
+
     return render_template("home.html", produtos=produtos)
 
 
@@ -49,19 +71,37 @@ def add():
         categoria = request.form.get("categoria")
         if not categoria:
             return error("Categoria inválida", 403)
-        # unir todos os dados em um tuple
-        dadosNovoProduto = (nome.title(), quantidade, valor, categoria)
+        # unir todos os dados em um dict
+        dadosNovoProduto = {
+            "nome": nome.title(), 
+            "quantidade": quantidade, 
+            "valor": valor, 
+            "categoria": categoria
+        }
+
+        # verificar se item já existe
+        stmt1 = select(produtosDb)
+        with engine.connect() as conn:
+            try:
+                produtos = conn.execute(stmt1).fetchall()
+            except:
+                return redirect("/")
+            # loop nos produtos para achar cópia
+            for produto in produtos:
+                    if dadosNovoProduto["nome"] == produto[1]:
+                        flash(f'Erro: Produto com nome "{dadosNovoProduto["nome"]}" já existe')
+                        return redirect("/")
+
+        # inserir no banco de dados caso não exista
+            stmt2 = insert(produtosDb).values(dadosNovoProduto)
+            try:
+                conn.execute(stmt2)
+                conn.commit()
+            except SQLAlchemyError as e:
+                flash(f"Ocorreu um erro ao adicionar produto: {e}")
+                return redirect("/")
         
-        # inserir no banco de dados
-        query = "INSERT INTO produtos (nome, quantidade, valor, categoria) VALUES (%s, %s, %s, %s)"
-        try:
-            cursor.execute(query, dadosNovoProduto)
-        except:
-            flash("Erro: ocorreu um erro ao adicionar o produto no banco de dados. Tente novamente.")
-            db.rollback()
-            return redirect("/")
-            
-        db.commit()
+        # notificar sucesso e redirecionar para home
         flash("Produto adicionado com sucesso.")
         return redirect("/")
     else:
@@ -76,15 +116,15 @@ def delete():
         return error("Id inválido", 403)
     
     # deletando do banco de dados
-    query = "DELETE FROM produtos WHERE id = %s"
-    try:
-        cursor.execute(query, (id,))
-    except:
-        flash("Erro: ocorreu um erro ao excluir produto. Tente novamente.")
-        db.rollback()
-        return redirect("/")
-    
-    db.commit()
+    stmt = produtosDb.delete().where(produtosDb.c.id == id)
+    with engine.connect() as conn:
+        try:
+            conn.execute(stmt)
+            conn.commit()
+        except SQLAlchemyError as e:
+            flash(f"Ocorreu um erro ao deletar produto: {e}")
+            return redirect("/")
+
     flash("Produto excluído com sucesso.")
     return redirect("/")
 
@@ -112,17 +152,33 @@ def edit():
         if not categoria:
             return error("Categoria inválida", 403)
         # união de todos os dados para a query
-        dadosProdutoEditado = (nome.title(), quantidade, valor, categoria, id)
+        dadosProdutoEditado = {"nome": nome.title(), 
+                               "quantidade": quantidade, 
+                               "valor": valor, 
+                               "categoria": categoria, 
+                               "id": id}
         # editando o banco de dados
-        query = "UPDATE produtos SET nome = %s, quantidade = %s, valor = %s, categoria = %s WHERE id = %s"
-        try:
-            cursor.execute(query, dadosProdutoEditado)
-        except:
-            flash("Erro: ocorreu um erro ao editar produto. Tente novamente")
-            db.rollback()
-            return redirect("/")
 
-        db.commit()
+        stmt1 = select(produtosDb)
+        with engine.connect() as conn:
+            try:
+                produtos = conn.execute(stmt1).fetchall()
+            except:
+                return redirect("/")
+            for produto in produtos:
+                if dadosProdutoEditado["nome"] == produto[1]:
+                    flash(f'Erro: Produto com nome "{dadosProdutoEditado["nome"]}" já existe')
+                    return redirect("/")
+
+        # editar produto caso nome novo não exista no banco de dados
+            stmt2 = update(produtosDb).where(produtosDb.c.id == id).values(dadosProdutoEditado)
+            try:
+                conn.execute(stmt2)
+                conn.commit()
+            except SQLAlchemyError as e:
+                flash(f"Ocorreu um erro ao editar produto: {e}")
+                return redirect("/")
+
         flash("Produto editado com sucesso.")
         return redirect("/")
     else:
@@ -132,33 +188,38 @@ def edit():
             return error("Id inválido: deve ser maior que 0", 403)
         
         # buscando valores atuais do produto
-        query = "SELECT id, nome, quantidade, valor, categoria FROM produtos WHERE id = %s"
-        try:
-            cursor.execute(query, (id,))
-        except:
-            flash("Erro: produto não encontrado no banco de dados.")
-            return redirect("/")
-    
-        produto = cursor.fetchall()
-        return render_template("edit.html", produto=produto[0])
+        stmt = select(produtosDb).where(produtosDb.c.id == id)
+        with engine.connect() as conn:
+            try:
+                produto = conn.execute(stmt).fetchone()
+            except SQLAlchemyError as e:
+                flash(f"Ocorreu um erro ao buscar produto: {e}")
+                return redirect("/")
+        return render_template("edit.html", produto=produto)
     
 
 @app.route("/search")
 def search():
     q = request.args.get("q")
-    query = "SELECT * FROM produtos WHERE id = %s OR nome LIKE %s OR categoria LIKE %s"
     if q:
-        try:
-            cursor.execute(query, (q, q, q,))
-        except:
-            flash(f'Erro: não foi possível pesquisar por "{q}"')
-            return redirect("/")
+        stmt = select(produtosDb).where(
+            or_(
+                (produtosDb.c.id.like(q)),
+                (produtosDb.c.nome.like(q)),
+                (produtosDb.c.categoria.like(q))
+            )
+        )
+        with engine.connect() as conn:
+            try:
+                produtos = conn.execute(stmt).fetchall()
+            except SQLAlchemyError as e:
+                flash(f"Ocorreu um erro ao buscar produto: {e}")
 
-        produtos = cursor.fetchall()
         return render_template("home.html", produtos=produtos, search=q)
 
     return redirect("/")
 
 
 if __name__ == "__main__":
+    db.create_all(bind=engine)
     app.run(debug=True)
